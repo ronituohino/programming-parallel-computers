458be90f4782168c27ed8c461878d7cb9e145a6c
Roni Tuohino <tuohinoroni@gmail.com>
2024-11-04 13:40:17 +0200

    Finish IS2

--- is2/is.cc
+++ is2/is.cc
@@ -23,24 +23,24 @@ struct Sum
 Sum get_rect_sum(vector<double> const &table, int nx, int x0, int x1, int y0, int y1)
 {
     double sum_to_rect_br[3] = {
-        table[0 + 3 * x1 + 3 * nx * y1],
-        table[1 + 3 * x1 + 3 * nx * y1],
-        table[2 + 3 * x1 + 3 * nx * y1]};
+        table[0 + 3 * (x1 - 1) + 3 * nx * (y1 - 1)],
+        table[1 + 3 * (x1 - 1) + 3 * nx * (y1 - 1)],
+        table[2 + 3 * (x1 - 1) + 3 * nx * (y1 - 1)]};
 
     double sum_to_rect_tl[3] = {
-        x0 > 0 ? table[0 + 3 * (x0 - 1) + 3 * nx * y0] : 0.0,
-        x0 > 0 ? table[1 + 3 * (x0 - 1) + 3 * nx * y0] : 0.0,
-        x0 > 0 ? table[2 + 3 * (x0 - 1) + 3 * nx * y0] : 0.0};
+        (x0 > 0 && y0 > 0) ? table[0 + 3 * (x0 - 1) + 3 * nx * (y0 - 1)] : 0.0,
+        (x0 > 0 && y0 > 0) ? table[1 + 3 * (x0 - 1) + 3 * nx * (y0 - 1)] : 0.0,
+        (x0 > 0 && y0 > 0) ? table[2 + 3 * (x0 - 1) + 3 * nx * (y0 - 1)] : 0.0};
 
     double sum_to_rect_tr[3] = {
-        y0 > 0 ? table[0 + 3 * x1 + 3 * nx * (y0 - 1)] : 0.0,
-        y0 > 0 ? table[1 + 3 * x1 + 3 * nx * (y0 - 1)] : 0.0,
-        y0 > 0 ? table[2 + 3 * x1 + 3 * nx * (y0 - 1)] : 0.0};
+        y0 > 0 ? table[0 + 3 * (x1 - 1) + 3 * nx * (y0 - 1)] : 0.0,
+        y0 > 0 ? table[1 + 3 * (x1 - 1) + 3 * nx * (y0 - 1)] : 0.0,
+        y0 > 0 ? table[2 + 3 * (x1 - 1) + 3 * nx * (y0 - 1)] : 0.0};
 
     double sum_to_rect_bl[3] = {
-        x0 > 0 ? table[0 + 3 * (x0 - 1) + 3 * nx * y1] : 0.0,
-        x0 > 0 ? table[1 + 3 * (x0 - 1) + 3 * nx * y1] : 0.0,
-        x0 > 0 ? table[2 + 3 * (x0 - 1) + 3 * nx * y1] : 0.0};
+        x0 > 0 ? table[0 + 3 * (x0 - 1) + 3 * nx * (y1 - 1)] : 0.0,
+        x0 > 0 ? table[1 + 3 * (x0 - 1) + 3 * nx * (y1 - 1)] : 0.0,
+        x0 > 0 ? table[2 + 3 * (x0 - 1) + 3 * nx * (y1 - 1)] : 0.0};
 
     Sum sum_for_rect = {
         sum_to_rect_br[0] -
@@ -92,6 +92,12 @@ Result segment(int ny, int nx, const float *data)
                 total_g += sums_of_pixels[1 + 3 * x + 3 * nx * (y - 1)];
                 total_b += sums_of_pixels[2 + 3 * x + 3 * nx * (y - 1)];
             }
+            if (x > 0 && y > 0)
+            {
+                total_r -= sums_of_pixels[0 + 3 * (x - 1) + 3 * nx * (y - 1)];
+                total_g -= sums_of_pixels[1 + 3 * (x - 1) + 3 * nx * (y - 1)];
+                total_b -= sums_of_pixels[2 + 3 * (x - 1) + 3 * nx * (y - 1)];
+            }
 
             sums_of_pixels[0 + 3 * x + 3 * nx * y] = total_r;
             sums_of_pixels[1 + 3 * x + 3 * nx * y] = total_g;
@@ -108,14 +114,14 @@ Result segment(int ny, int nx, const float *data)
     // After full scan of the original image, make the window smaller.
 
     Sum sum_for_img = {
-        sums_of_pixels[ny * (nx - 1) * 3 + 0],
-        sums_of_pixels[ny * (nx - 1) * 3 + 1],
-        sums_of_pixels[ny * (nx - 1) * 3 + 2]};
+        sums_of_pixels[ny * nx * 3 - 3],
+        sums_of_pixels[ny * nx * 3 - 2],
+        sums_of_pixels[ny * nx * 3 - 1]};
 
     Sum sq_sum_for_img = {
-        sq_sums_of_pixels[ny * (nx - 1) * 3 + 0],
-        sq_sums_of_pixels[ny * (nx - 1) * 3 + 1],
-        sq_sums_of_pixels[ny * (nx - 1) * 3 + 2]};
+        sq_sums_of_pixels[ny * nx * 3 - 3],
+        sq_sums_of_pixels[ny * nx * 3 - 2],
+        sq_sums_of_pixels[ny * nx * 3 - 1]};
 
     int img_dim = nx * ny;
 
@@ -126,68 +132,59 @@ Result segment(int ny, int nx, const float *data)
     {
         for (int w = nx; w > 0; w--)
         {
+            if (w == nx && h == ny)
+            {
+                continue;
+            }
             int rect_dim = w * h;
+            int out_dim = img_dim - rect_dim;
             for (int y0 = 0; y0 <= ny - h; y0++)
             {
                 for (int x0 = 0; x0 <= nx - w; x0++)
                 {
-                    int x1 = x0 + w - 1;
-                    int y1 = y0 + h - 1;
-
-                    cout << "Handling this: ";
-                    cout << x0 << x1 << y0 << y1 << endl;
-                    cout << w << h;
-                    cout << endl;
+                    int x1 = x0 + w;
+                    int y1 = y0 + h;
 
                     Sum sum_for_rect = get_rect_sum(sums_of_pixels, nx, x0, x1, y0, y1);
                     Sum sq_sum_for_rect = get_rect_sum(sq_sums_of_pixels, nx, x0, x1, y0, y1);
 
-                    double inner_avg_col[3] = {
-                        sum_for_rect.r / rect_dim,
-                        sum_for_rect.g / rect_dim,
-                        sum_for_rect.b / rect_dim};
+                    double error1 = (sq_sum_for_rect.r - 2 * (sum_for_rect.r / rect_dim) * sum_for_rect.r + rect_dim * pow(sum_for_rect.r / rect_dim, 2) +
+                                     sq_sum_for_rect.g - 2 * (sum_for_rect.g / rect_dim) * sum_for_rect.g + rect_dim * pow(sum_for_rect.g / rect_dim, 2) +
+                                     sq_sum_for_rect.b - 2 * (sum_for_rect.b / rect_dim) * sum_for_rect.b + rect_dim * pow(sum_for_rect.b / rect_dim, 2));
 
-                    double outer_avg_col[3] = {
-                        (sum_for_img.r - sum_for_rect.r) / img_dim,
-                        (sum_for_img.g - sum_for_rect.g) / img_dim,
-                        (sum_for_img.b - sum_for_rect.b) / img_dim};
+                    Sum sum_for_out = {
+                        (sum_for_img.r - sum_for_rect.r),
+                        (sum_for_img.g - sum_for_rect.g),
+                        (sum_for_img.b - sum_for_rect.b)};
+                    Sum sq_sum_for_out = {
+                        (sq_sum_for_img.r - sq_sum_for_rect.r),
+                        (sq_sum_for_img.g - sq_sum_for_rect.g),
+                        (sq_sum_for_img.b - sq_sum_for_rect.b)};
 
-                    double error1 = (sq_sum_for_rect.r - 2 * inner_avg_col[0] * sum_for_rect.r + rect_dim * pow(inner_avg_col[0], 2) +
-                                     sq_sum_for_rect.g - 2 * inner_avg_col[1] * sum_for_rect.g + rect_dim * pow(inner_avg_col[1], 2) +
-                                     sq_sum_for_rect.b - 2 * inner_avg_col[2] * sum_for_rect.b + rect_dim * pow(inner_avg_col[2], 2));
-
-                    double error2 = (sq_sum_for_img.r - 2 * outer_avg_col[0] * sum_for_img.r + img_dim * pow(outer_avg_col[0], 2) +
-                                     sq_sum_for_img.g - 2 * outer_avg_col[1] * sum_for_img.g + img_dim * pow(outer_avg_col[1], 2) +
-                                     sq_sum_for_img.b - 2 * outer_avg_col[2] * sum_for_img.b + img_dim * pow(outer_avg_col[2], 2));
+                    double error2 = (sq_sum_for_out.r - 2 * (sum_for_out.r / out_dim) * sum_for_out.r + out_dim * pow(sum_for_out.r / out_dim, 2) +
+                                     sq_sum_for_out.g - 2 * (sum_for_out.g / out_dim) * sum_for_out.g + out_dim * pow(sum_for_out.g / out_dim, 2) +
+                                     sq_sum_for_out.b - 2 * (sum_for_out.b / out_dim) * sum_for_out.b + out_dim * pow(sum_for_out.b / out_dim, 2));
 
                     double cost = error1 + error2;
 
-                    cout << "Cost: ";
-                    cout << cost << endl;
-
-                    if (cost < min_cost)
+                    if (cost > 0 && cost < min_cost)
                     {
+                        min_cost = cost;
                         min = {
                             y0,
                             x0,
                             y1,
                             x1,
-                            {(float)outer_avg_col[0], (float)outer_avg_col[1], (float)outer_avg_col[2]},
-                            {(float)inner_avg_col[0], (float)inner_avg_col[1], (float)inner_avg_col[2]}};
+                            {(float)(sum_for_out.r / out_dim),
+                             (float)(sum_for_out.g / out_dim),
+                             (float)(sum_for_out.b / out_dim)},
+                            {(float)(sum_for_rect.r / rect_dim),
+                             (float)(sum_for_rect.g / rect_dim),
+                             (float)(sum_for_rect.b / rect_dim)}};
                     }
                 }
             }
         }
     }
     return min;
-}
-
-int main()
-{
-    float data[9] = {
-        +0.31532657, +0.11138125, +0.19794576,
-        +0.31532657, +0.11138125, +0.74090004,
-        +0.31532657, +0.11138125, +0.19794576};
-
-    segment(1, 3, data);
 }
