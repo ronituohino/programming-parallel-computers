af31fd755e90633a8bc144a7d6bb6c8095c15cc6
Roni Tuohino <tuohinoroni@gmail.com>
2025-01-12 16:44:34 +0200

    Solve so4 in under 2.2s (2pts)

--- so4/so.cc
+++ so4/so.cc
@@ -6,46 +6,116 @@ typedef unsigned long long data_t;
 
 using namespace std;
 
-void sort_in_vector(int len, data_t *data, vector<data_t> &output)
+/*
+void psort(int n, data_t *data)
 {
-    for (int v = 0; v < len; v++)
+    data_t *buffer = (data_t *)malloc(n * sizeof(data_t));
+    // cout << *(data + i) << " " << endl;
+    constexpr int vals_to_compute = 64; // even
+    int slots = n / vals_to_compute + 1;
+
+    // disassemble data into pairs, and sort them in place
+    for (int i = 0; i < slots; i++)
     {
-        data_t d = *data;
-        output[v] = d;
+        int start = i * vals_to_compute;
+        int end = min(start + vals_to_compute, n);
+
+        for (int v = 0; v < vals_to_compute; v += 2)
+        {
+            cout << i << " " << start << " " << end << endl;
+            sort(data + start, data + end);
+        }
     }
-    sort(output.begin(), output.end());
+
+    free(buffer);
+}
+*/
+
+void merge(data_t *data, int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    data_t *leftBuff = (data_t *)malloc(n1 * sizeof(data_t));
+    data_t *rightBuff = (data_t *)malloc(n2 * sizeof(data_t));
+
+    for (int i = 0; i < n1; ++i)
+    {
+        leftBuff[i] = data[left + i];
+    }
+    for (int i = 0; i < n2; ++i)
+    {
+        rightBuff[i] = data[mid + 1 + i];
+    }
+
+    // Merge the buffers back into the original data
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2)
+    {
+        if (leftBuff[i] <= rightBuff[j])
+        {
+            data[k] = leftBuff[i];
+            i++;
+        }
+        else
+        {
+            data[k] = rightBuff[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of leftBuff
+    while (i < n1)
+    {
+        data[k] = leftBuff[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of rightBuff
+    while (j < n2)
+    {
+        data[k] = rightBuff[j];
+        j++;
+        k++;
+    }
+
+    free(leftBuff);
+    free(rightBuff);
+}
+
+void pms(data_t *data, int left, int right, int thresh)
+{
+    // Subsets with only 1 element are already "sorted"
+    if (left < right)
+    {
+        int len = right - left;
+        int mid = left + len / 2;
+
+        if (len < thresh)
+        {
+            // For small subsets, use serial sort
+            sort(data + left, data + right + 1);
+        }
+        else
+        {
+#pragma omp parallel sections
+            {
+#pragma omp section
+                pms(data, left, mid, thresh);
+
+#pragma omp section
+                pms(data, mid + 1, right, thresh);
+            }
+        }
+
+        merge(data, left, mid, right);
+    }
 }
 
 void psort(int n, data_t *data)
 {
-    constexpr int parts = 2;
-    int l = n / parts; // the approx length of each sub-array
-    int approx_total = l * parts;
-    int leftover = n - approx_total; // how many elements were left over
-
-    vector<data_t> res1; // the first array has the extra elements (l + leftover)
-    vector<data_t> res2; // the rest are the same size (l)
-
-    cout << data << " " << endl;
-    cout << *data << " " << endl;
-    data++;
-    cout << *data << " " << endl;
-
-    // disassemble data into parts, and sort them in those parts
-    for (int i = 0; i < parts; i++)
-    {
-        int start = (i * l) + leftover;
-        int end = ((i + 1) * l) + leftover - 1;
-        if (i == 0)
-        {
-            start = 0;
-        }
-        int len = end - start;
-
-        // sort_in_vector(len, data, res1);
-    }
-
-    // assemble sorted parts back into data
-    // cout << res1[0] << " " << res1[1] << endl;
-    // cout << res2[0] << " " << res2[1] << endl;
+    int thresh = 100000;
+    pms(data, 0, n - 1, thresh);
 }
